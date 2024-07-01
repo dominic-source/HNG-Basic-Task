ec469237eaa994d3513f76e9a3856125126935ee
dominic-source <chinonsodomnic@gmail.com>
2024-07-01 21:48:08 +0100

    create a basic node app

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const geoip = require('geoip-lite');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/api/hello', (req, res) => {
+  const visitor_name = req.query.visitor_name;
+  const ip = req.ip;
+  let obj, city;
+  try {
+    city = geoip.lookup(ip).city;
+    obj = {
+      "client_ip": ip,
+      "location": city,
+      "greeting": `Hello, ${visitor_name}!, the temperature is 11 degrees Celcius in ${city}`
+      }
+  } catch (error) {
+    res.status(500).send('Ip address not found!');
+  }
+  res.status(200).json(obj);
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
