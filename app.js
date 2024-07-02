c650a44467ee162ac9889dda75423f34d34bbfc1
dominic-source <chinonsodomnic@gmail.com>
2024-07-02 15:37:39 +0100

    debug code and resolve issues v3

--- app.js
+++ app.js
@@ -11,36 +11,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/hello', async (req, res) => {
   const visitor_name = req.query.visitor_name || 'Unknown';
   const apiKey = process.env.apiKey;
-  const ipBaseURL = 'http://api.weatherapi.com/v1/ip.json';
   const weatherBaseURL = 'http://api.weatherapi.com/v1/current.json';
-
-   // Get the client's IP address
   const clientIp = req.headers['x-forwarded-for'] || req.ip;
+  // const clientIp = "41.203.78.171";
   try {
-    // Get the IP address and location data
-    const response = await axios.get(ipBaseURL, {
-      params: {
-        key: apiKey,
-        q: clientIp,
-      }
-    });
-
-    const data = response.data;
-    const city = data.city;
-    const lat = data.lat;
-    const lon = data.lon;
-    // const ip = data.ip;
-
     // Get the weather data
     const weatherResponse = await axios.get(weatherBaseURL, {
       params: {
         key: apiKey,
-        q: `${lat},${lon}`,
+        q: clientIp,
         current_fields: "temp_c" 
       }
     });
+
     // Temperature in Celsius
-    const temperature = weatherResponse.data.current.temp_c;
+    const data = weatherResponse.data;
+    const temperature = data.current.temp_c;
+    const city = data.location.name;
+
     const obj = {
       "client_ip": clientIp,
       "location": city,
